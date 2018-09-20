af542f15c23432925a2febb5e8e9f4788432f474
gesparza3 <gesparza3@mail.csuchico.edu>
2018-09-20 14:32:26 -0700

    Added setup files using cookiecutter to create package

---
+++ marky/README.md
@@ -0,0 +1,21 @@
+# Marky
+
+Converts markdown files and inserts into user-provided template.
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ marky --help
+
--- marky/marky/cli.py
+++ marky/marky/cli.py
@@ -4,6 +4,7 @@ import mistune ## Library for converting .md to .html
 import re
 import click
 
+__author__ = "Grant Esparza"
 
 def convert(m_file):
     
@@ -26,8 +27,13 @@ def insert(cm_file, h_file, dest_file):
 @click.argument('markdown', click.File('rb'), required=1)
 @click.argument('template', click.File('rb'), required=1)
 @click.argument('destination', click.File('w'), required=1)
-def main(markdown, template, destination):
+@click.option('--markdown', help="A markdown(.md) file.")
+@click.option('--template', help="A html template, indicate where to put content by placing '<!-- Markdown Section -->' at desired location.")
+@click.option('--destination', help="Desired location for new html file.")
 
+def main(markdown, template, destination):
+    
+    """A simple program to take a markdown file and insert contents into an html template"""
     ## Open files to read
     m_file = open(markdown, "r")
     h_file = open(template, "r")
